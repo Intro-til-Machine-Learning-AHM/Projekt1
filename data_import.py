3ce1b12018e8d6bbfa20d880afed5a3382baba2b
Marcus Medom Ryding <mrok4a@gmail.com>
2018-02-25 21:08:35 +0100

    Don't let insulin be zer

--- data_import.py
+++ data_import.py
@@ -5,6 +5,6 @@ names = ["pregs", "glucose", "bp", "thickness", "insulin", "bmi", "dia_pedig", "
 data_raw = pd.read_csv("diadata.csv", header=None, names=names)
 
 # Delete rows with missing data
-non_zero = ["glucose", "bp", "thickness", "bmi"]
+non_zero = ["glucose", "bp", "thickness", "bmi", "insulin"]
 mask = np.logical_not(np.any(data_raw[non_zero] == 0,1))
 data = data_raw[mask]
