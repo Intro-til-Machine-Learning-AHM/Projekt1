b05b2198149bf56150d9fbea347e21f8946a5346
Andreas Egelund Mortensen <andreas.egelund@live.dk>
2018-02-25 19:39:13 +0100

    summary statistics

--- visualization.py
+++ visualization.py
@@ -14,12 +14,26 @@ from data_import import data
 # Load data
 df=pd.read_csv('diadata.csv', sep=',',header=None)
 df.values
-np.mean(data['pregs'])
-np.mean(data['glucose'])
-np.mean(data['bp'])
-np.mean(data['thickness'])
-np.mean(data['insulin'])
-np.mean(data['bmi'])
-np.mean(data['dia_pedig'])
-np.mean(data['age'])
+# Assign mean values
+print(np.mean(data['pregs']))
+print(np.mean(data['glucose']))
+print(np.mean(data['bp']))
+print(np.mean(data['thickness']))
+print(np.mean(data['insulin']))
+print(np.mean(data['bmi']))
+print(np.mean(data['dia_pedig']))
+print(np.mean(data['age']))
+# Assign variance
+np.var(data['pregs'])
+np.var(data['glucose'])
+np.var(data['bp'])
+np.var(data['thickness'])
+np.var(data['insulin'])
+np.var(data['bmi'])
+np.var(data['dia_pedig'])
+np.var(data['age'])
+# Assign covariance
+covmatrix=np.cov(data)
 # Basic summary statistics
+#print(data.describe())
+print(covmatrix[0])
