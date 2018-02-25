9609887dd2a2267a8766afa36cbdad6f8c1f7db1
Andreas Egelund Mortensen <andreas.egelund@live.dk>
2018-02-25 18:16:53 +0100

    Start of exercise 2

--- visualization.py
+++ visualization.py
@@ -5,14 +5,21 @@ Created on Tue Feb 20 15:15:00 2018
 @author: andre
 """
 
-# Import packagaes
+# Import packagaes and data
 import sys
 import os
 import numpy as np
 import pandas as pd
-Load data
+from data_import import data
+# Load data
 df=pd.read_csv('diadata.csv', sep=',',header=None)
 df.values
-print(df)
-print(os.getcwd())
-print(np.array([1,2,3]))
+np.mean(data['pregs'])
+np.mean(data['glucose'])
+np.mean(data['bp'])
+np.mean(data['thickness'])
+np.mean(data['insulin'])
+np.mean(data['bmi'])
+np.mean(data['dia_pedig'])
+np.mean(data['age'])
+# Basic summary statistics
