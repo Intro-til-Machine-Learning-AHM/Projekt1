680d9ca94c0c6fdf72eed1ff241e454ccf05702d
Andreas Egelund Mortensen <andreas.egelund@live.dk>
2018-02-26 21:32:37 +0100

    exercise 3 5-6

--- pca.py
+++ pca.py
@@ -17,6 +17,13 @@ u, s, vh = np.linalg.svd(data_cleaned)
 v = vh.T
 
 d_pr = data_cleaned @ v
-plt.plot(d_pr[class_var == 0,0], d_pr[class_var == 0,1], '.')
-plt.plot(d_pr[class_var == 1,0], d_pr[class_var == 1,1], '.')
+#plt.plot(d_pr[class_var == 0,0], d_pr[class_var == 0,1], '.')
+#plt.plot(d_pr[class_var == 1,0], d_pr[class_var == 1,1], '.')
+#plt.show()
+plt.figure()
+plt.ylabel('Explained variance')
+plt.xlabel('Principal components')
+plt.axis([-0.3, 7, 0, 1.2])
+plt.plot(np.cumsum(s*s)/np.sum(s*s))
 plt.show()
+print(np.sum(s[0:2]**2)/(np.sum(s**2)))
