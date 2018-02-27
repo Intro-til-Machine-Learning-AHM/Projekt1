e1f8ed35ccf2e623d7777068304c89364e1592c3
Andreas Egelund Mortensen <andreas.egelund@live.dk>
2018-02-27 10:01:30 +0100

    histograms

--- visualization.py
+++ visualization.py
@@ -44,4 +44,42 @@ Covariance=data.corr().round(2)
 # Boxplots code
 data_copy = data.copy()
 del data_copy["class"]
-plt.boxplot(data_copy.as_matrix())
+#plt.boxplot(data_copy.as_matrix())
+range_data = data.max()-data.min()
+#print(range_data)
+fig = plt.figure()
+#plt.hist(data_copy["pregs"],bins=fig = plt.figure())
+
+ax1 = fig.add_subplot(421)
+ax1.set_title("pregs")
+plt.hist(data_copy["pregs"],bins=17)
+
+ax2 = fig.add_subplot(422)
+ax2.set_title("glucose")
+plt.hist(data_copy["glucose"])
+
+ax3 = fig.add_subplot(423)
+ax3.set_title("bp")
+plt.hist(data_copy["bp"])
+
+ax4 = fig.add_subplot(424)
+ax4.set_title("thickness")
+plt.hist(data_copy["thickness"])
+
+ax5 = fig.add_subplot(425)
+ax5.set_title("insulin")
+plt.hist(data_copy["insulin"])
+
+ax6 = fig.add_subplot(426)
+ax6.set_title("bmi")
+plt.hist(data_copy["bmi"])
+
+ax7 = fig.add_subplot(427)
+ax7.set_title("dia_pedig")
+plt.hist(data_copy["dia_pedig"])
+
+ax8 = fig.add_subplot(428)
+ax8.set_title("age")
+plt.hist(data_copy["age"])
+
+plt.show()
