772da0354ad29cd21ecd8f7e11c7f6df2c298276
Hilde^u^ <hildan94@hotmail.com>
2018-02-27 11:11:55 +0100

    Histograms

--- visualization.py
+++ visualization.py
@@ -12,6 +12,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from data_import import data
+from scipy.stats import norm
+from scipy import stats
 # Load data
 df=pd.read_csv('diadata.csv', sep=',',header=None)
 df.values
@@ -50,36 +52,16 @@ range_data = data.max()-data.min()
 fig = plt.figure()
 #plt.hist(data_copy["pregs"],bins=fig = plt.figure())
 
-ax1 = fig.add_subplot(421)
-ax1.set_title("pregs")
-plt.hist(data_copy["pregs"],bins=17)
-
-ax2 = fig.add_subplot(422)
-ax2.set_title("glucose")
-plt.hist(data_copy["glucose"])
-
-ax3 = fig.add_subplot(423)
-ax3.set_title("bp")
-plt.hist(data_copy["bp"])
-
-ax4 = fig.add_subplot(424)
-ax4.set_title("thickness")
-plt.hist(data_copy["thickness"])
-
-ax5 = fig.add_subplot(425)
-ax5.set_title("insulin")
-plt.hist(data_copy["insulin"])
-
-ax6 = fig.add_subplot(426)
-ax6.set_title("bmi")
-plt.hist(data_copy["bmi"])
-
-ax7 = fig.add_subplot(427)
-ax7.set_title("dia_pedig")
-plt.hist(data_copy["dia_pedig"])
-
-ax8 = fig.add_subplot(428)
-ax8.set_title("age")
-plt.hist(data_copy["age"])
+for c in data.columns:
+    ax = fig.add_subplot(421)
+    ax.set_title(c)
+    plt.hist(data[c],bins=17, normed=True)
+    (mu,sigma) = norm.fit(data[c])
+    xt = plt.xticks()[0]
+    xmin, xmax = min(xt), max(xt)
+    lnspc = np.linspace(xmin, xmax, len(data[c]))
+    pdf_g = stats.norm.pdf(lnspc, mu, sigma)
+    plt.plot(lnspc, pdf_g, label="Norm")
+    plt.show()
 
 plt.show()
